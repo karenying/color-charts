ac85d33d0084bdc96a9be1eaa32014164e6ff68e
Karen Ying <karenying7@gmail.com>
2020-03-23 12:01:12 -0400

    Add hex constants

---
+++ colors.js
@@ -0,0 +1,13 @@
+export const ORANGE = "E69F00";
+
+export const LIGHT_BLUE = "56B4E9";
+
+export const GREEN = "009E73";
+
+export const YELLOW = "F0E442";
+
+export const RED = "D55E00";
+
+export const PINK = "CC79A7";
+
+export const BLACK = "000000";
