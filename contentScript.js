04419c5416c0f387c7523e68e02f1c7aaf0e1990
Karen Ying <karenying7@gmail.com>
2020-03-23 11:50:46 -0400

    Change all images to base64 using HTML canvas

--- contentScript.js
+++ contentScript.js
@@ -1,29 +1,17 @@
-/*
-const desaturateImage = function(image) {
-    var canvas = document.createElement("canvas");
-    image.parentNode.insertBefore(canvas, image);
-    canvas.width = image.width;
-    canvas.height = image.height;
-    image.parentNode.removeChild(image);
-
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(image, 0, 0, image.width * image.width / image.naturalWidth, image.height * image.height / image.naturalHeight);
-    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    var data = imgData.data;
-
-    for (var i = 0; i < data.length; i += 4) {
-        var grey =
-            0.2126 * data[i] + 0.7152 * data[i + 1] + 0.0722 * data[i + 2];
-        data[i] = grey;
-        data[i + 1] = grey;
-        data[i + 2] = grey;
-    }
-    ctx.putImageData(imgData, 0, 0, 0, 0, canvas.width, canvas.height);
-} */
-
 let images = document.getElementsByTagName('img');
+console.log(images);
 
 for (let i = 0; i < images.length; i++) {
-    console.log($(images[i]).css());
-    // desaturateImage(images[i]);
+    let base64_img = new Image();
+	base64_img.src = images[i].src;
+
+	base64_img.onload = () => {
+		var canvas = document.createElement("canvas");
+		var ctx = canvas.getContext("2d");
+		canvas.width = base64_img.naturalWidth;
+		canvas.height = base64_img.naturalHeight;
+        ctx.drawImage(base64_img, 0, 0);
+        var base64_url = canvas.toDataURL();
+        images[i].src = base64_url;
+    };
 }
