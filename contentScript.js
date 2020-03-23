a14542d5e0a17fd30791a18c6bf50f007a4bd2aa
Karen Ying <karenying7@gmail.com>
2020-03-23 12:02:58 -0400

    Format contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -6,11 +6,13 @@ for (let i = 0; i < images.length; i++) {
 	base64_img.src = images[i].src;
 
 	base64_img.onload = () => {
-		var canvas = document.createElement("canvas");
+        var canvas = document.createElement("canvas");
+        canvas.width = base64_img.naturalWidth;
+        canvas.height = base64_img.naturalHeight;
+        
 		var ctx = canvas.getContext("2d");
-		canvas.width = base64_img.naturalWidth;
-		canvas.height = base64_img.naturalHeight;
         ctx.drawImage(base64_img, 0, 0);
+        
         var base64_url = canvas.toDataURL();
         images[i].src = base64_url;
     };
