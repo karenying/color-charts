ed55e7ca83cf49af8f3c17616cf8ac6fed29ff29
Karen Ying <karenying7@gmail.com>
2020-03-24 10:45:56 -0400

    Add DARK_BLUE

--- contentScript.js
+++ contentScript.js
@@ -4,9 +4,10 @@ const LIGHT_BLUE = [86, 180, 233];
 const GREEN = [0, 158, 115];
 const YELLOW = [240, 228, 66];
 const RED = [213, 94, 0];
+const DARK_BLUE = [0, 114, 178]
 const PINK = [204, 121, 167];
 const BLACK = [0, 0, 0];
-const COLORBLIND_FRIENDLY_COLORS = [ORANGE, LIGHT_BLUE, GREEN, YELLOW, RED, PINK, BLACK];
+const COLORBLIND_FRIENDLY_COLORS = [ORANGE, LIGHT_BLUE, GREEN, YELLOW, RED, DARK_BLUE, PINK, BLACK];
 // -------------------------------------------------------------------------
 // get all images
 let images = document.getElementsByTagName('img');
