e3349989de9725168ebabf60b8143aef9e245a47
Karen Ying <karenying7@gmail.com>
2020-03-24 18:37:22 -0400

    Add context menus

--- background.js
+++ background.js
@@ -1,4 +1,45 @@
 chrome.contextMenus.create({
     title: 'Color Charts',
-    contexts: ['image']
+    contexts: ['image'],
+    id: 'parent'
+});
+
+chrome.contextMenus.create({
+    title: 'None',
+    type: "radio",
+    contexts: ['image'],
+    parentId: 'parent',
+    checked: true, 
+});
+
+chrome.contextMenus.create({
+    title: 'Okabe and Ito',
+    type: "radio",
+    contexts: ['image'],
+    parentId: 'parent',
+    checked: false, 
+});
+
+chrome.contextMenus.create({
+    title: 'Tol (bright)',
+    type: "radio",
+    contexts: ['image'],
+    parentId: 'parent',
+    checked: false
+});
+
+chrome.contextMenus.create({
+    title: 'Tol (muted)',
+    type: "radio",
+    contexts: ['image'],
+    parentId: 'parent',
+    checked: false, 
+});
+
+chrome.contextMenus.create({
+    title: 'Tol (light)',
+    type: "radio",
+    contexts: ['image'],
+    parentId: 'parent',
+    checked: false, 
 });
