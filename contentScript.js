975eab9d30120943f1622fc14ce45c02ecf0607a
Karen Ying <karenying7@gmail.com>
2020-03-24 18:56:59 -0400

    Add amany more constants

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,47 @@
 // ------------------- color constants --------------------------------------
-const ORANGE = [230, 159, 0];
-const LIGHT_BLUE = [86, 180, 233];
-const GREEN = [0, 158, 115];
-const YELLOW = [240, 228, 66];
-const RED = [213, 94, 0];
-const DARK_BLUE = [0, 114, 178];
-const PINK = [204, 121, 167];
+const OI_ORANGE = [230, 159, 0];
+const OI_LIGHT_BLUE = [86, 180, 233];
+const OI_GREEN = [0, 158, 115];
+const OI_YELLOW = [240, 228, 66];
+const OI_RED = [213, 94, 0];
+const OI_DARK_BLUE = [0, 114, 178];
+const OI_PINK = [204, 121, 167];
 const BLACK = [0, 0, 0];
-const COLORBLIND_FRIENDLY_COLORS = [ORANGE, LIGHT_BLUE, GREEN, YELLOW, RED, DARK_BLUE, PINK, BLACK];
+
+const TB_PINK = [238, 102, 119];
+const TB_GREEN = [34, 136, 51];
+const TB_DARK_BLUE = [68, 119, 170];
+const TB_YELLOW = [204, 187, 68];
+const TB_LIGHT_BLUE = [102, 204, 238];
+const TB_PURPLE = [170, 51, 119];
+const TB_GRAY = [187, 187, 187];
+
+const TM_LIGHT_BLUE = [136, 204, 238];
+const TM_GREEN = [68, 170, 153];
+const TM_DARK_GREEN = [17, 119, 51];
+const TM_DARK_BLUE = [51, 34, 136];
+const TM_YELLOW = [221, 204, 119];
+const TM_DARK_YELLOW = [153, 153, 51];
+const TM_PINK = [204, 102, 119];
+const TM_DARK_PINK = [136, 34, 85];
+const TM_PURPLE = [170, 68, 153];
+const GRAY = [221, 221, 221];
+
+const TL_LIGHT_GREEN = [191, 196, 101];
+const TL_DARK_GREEN = [170, 170, 0];
+const TL_BLUE = [119, 170, 221];
+const TL_RED = [238, 136, 102];
+const TL_YELLOW = [238, 221, 136];
+const TL_PINK = [255, 170, 187];
+const TL_LIGHT_BLUE = [153, 221, 255];
+const TL_GREEN = [68, 187, 153];
+
+const OKABE_ITO = [OI_ORANGE, OI_LIGHT_BLUE, OI_GREEN, OI_YELLOW, OI_RED, OI_DARK_BLUE, OI_PINK, BLACK];
+const TOL_BRIGHT = [TB_PINK, TB_GREEN, TB_DARK_BLUE, TB_YELLOW, TB_LIGHT_BLUE, TB_PURPLE, TB_GRAY];
+const TOL_MUTED = [TM_LIGHT_BLUE, TM_GREEN, TM_DARK_GREEN, TM_DARK_BLUE, TM_YELLOW, TM_DARK_YELLOW, TM_DARK_PINK, TM_PURPLE, GRAY]
+const TOL_LIGHT = [TL_LIGHT_GREEN, TL_DARK_GREEN, TL_BLUE, TL_RED, TL_YELLOW, TL_PINK, TL_LIGHT_BLUE, TL_GREEN, GRAY];
+
+const COLORBLIND_FRIENDLY_COLORS = OKABE_ITO;
 // -------------------------------------------------------------------------
 // get all images
 let images = document.getElementsByTagName('img');
