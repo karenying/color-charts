0a7219720a2464a9ba35d2d2987837003ad7cec0
Karen Ying <karenying7@gmail.com>
2020-04-10 11:49:48 -0400

    Change to function expressions

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,5 @@
 // filter image
-function filter(info, tab) {
+filter = function (info, tab) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { url: info.srcUrl, palette: info.menuItemId }, function (response) { });
         return Promise.all('Go away error message!!');
@@ -7,7 +7,7 @@ function filter(info, tab) {
 }
 
 // create menu
-function createMenu() {
+createMenu = function () {
     const titles = ['None', 'Okabe and Ito', 'Tol (bright)', 'Tol (muted)', 'Tol (light)'];
 
     for (let i = 0; i < titles.length; i++) {
