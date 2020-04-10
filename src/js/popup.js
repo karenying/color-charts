4ab6709e8d48dba61c3511e211ce6b0abe296eef
Karen Ying <karenying7@gmail.com>
2020-04-10 08:48:52 -0400

    Add caching to preferences

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+    // open about link
     document.querySelector('#about').addEventListener("click", function () {
         if (chrome.runtime.openOptionsPage) {
             chrome.runtime.openOptionsPage();
@@ -9,21 +9,38 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     let palettes = document.getElementsByName('palette');
+
+    // get cached selected palette value and set radio button
+    chrome.storage.local.get(['paletteSelected'], function(result) {
+        let paletteSelected = result.paletteSelected;
+        for (let palette of palettes) {
+            if (palette.value === paletteSelected) {
+                palette.checked = true;
+            }
+        }
+    });
+
+    // update preferences on click
     for (let palette of palettes) {
         palette.onclick = function () {
-            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, { selected: palette.value }, function (response) { });
+            chrome.storage.local.set({'paletteSelected': palette.value}, function() {
+                console.log("Palette selected: " + palette.value);
             });
         }
     }
 
     let checkbox = document.getElementsByName('apply')[0];
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { applyAll: checkbox.checked }, function (response) { });
+    
+    // get cached apply all preference and set toggle
+    chrome.storage.local.get(['applyAll'], function(result) {
+        checkbox.checked = result.applyAll;
     });
+
+    // update preferences on click
     checkbox.onclick = function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { applyAll: checkbox.checked }, function (response) { });
+        chrome.storage.local.set({'applyAll': checkbox.checked}, function() {
+            console.log("Apply all turned " + value + ".");
         });
     }
+    
 }, false);
